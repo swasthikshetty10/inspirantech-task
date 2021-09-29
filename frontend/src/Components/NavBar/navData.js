fc987a18d0f55de60f56caf79ad7610d4243a014
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-09-29 21:26:07 +0530

    Navbar

---
+++ frontend/src/Components/NavBar/navData.js
@@ -0,0 +1,25 @@
+const navigations = [
+    {
+        name: "INDUSTRIES",
+        href: "#"
+    },
+    {
+        name: "SERVICES",
+        href: "#"
+    },
+    {
+        name: "PROJECTS",
+        href: "#"
+    },
+    {
+        name: "TRAININGS",
+        href: "#"
+    },
+    {
+        name: "CONTACTS",
+        href: "#"
+    },
+
+]
+
+export default navigations
