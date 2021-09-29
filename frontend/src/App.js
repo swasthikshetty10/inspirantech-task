7a36f99ae70173c270abf821aa5a4c708b2d1ed0
swasthikshetty10 <swasthikshetty10902@gmail.com>
2021-09-29 23:18:05 +0530

    about page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 
 import NavBar from "./Components/NavBar";
 import LandingPage from "./Components/LandingPage";
+import Mission from "./Components/Mission";
+import InfoPage from "./Components/InfoPage";
 function App() {
   return (
     <div className="App">
       <NavBar />
       <LandingPage />
+      <Mission />
+      <InfoPage />
     </div>
   );
 }
---
+++ frontend/src/Components/Mission/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import './mission.css'
+
+
+function Mission() {
+    return (
+        <section className="mission">
+            <h2>MISSION</h2>
+            <h1>We believe in purpose</h1>
+            <p>
+                Lorem ipsum dolor sit amet. Est accusantium harum quo minus omnis eum unde facere aut similique omnis est consectetur minima vel suscipit tempore. Est eligendi veniam est aliquam quae qui architecto porro! A doloremque magnam ea vero illo eum
+            </p>
+            <a className="btn btn-red">ABOUT US</a>
+        </section>
+    )
+}
+
+export default Mission
