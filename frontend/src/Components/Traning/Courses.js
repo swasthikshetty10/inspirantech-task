96b218fd8e5a5e288cba3126564fd1f6583e9eb4
swasthikshetty <swasthikshetty10902@gmail.com>
2021-10-04 00:27:39 +0530

    datafetch and render

--- frontend/src/Components/Traning/Courses.js
+++ frontend/src/Components/Traning/Courses.js
@@ -1,80 +1,52 @@
-import React from 'react'
 import './courses.css'
 import background from '../../Assets/images/engineers.jpg'
+import { useEffect, useState } from "react"
+import axios from "axios"
+
+const baseURL = "https://inspirante.herokuapp.com/api/trainings/"
 function Courses() {
+
+    const [courses, setCourse] = useState([])
+    const [loading, setLoading] = useState(true)
+    useEffect(() => {
+        setLoading(true)
+        axios.get(baseURL).then((response) => {
+            setCourse(response.data)
+        })
+        setLoading(false);
+    }, [])
+
     return (
         <section className="courses" >
-            <div className="grid-wrapper">
-                <div className="grid-card">
-                    <div className="industry-status">
-                        <a>INDUSTRY</a>
-                        <a>STATUS</a>
-                    </div>
-                    <div className="course-card" >
-                        <img src={background} />
-                        <div>
-                            <div className="course-details">
-                                <p>HAZOP Leadership Training For Engineers</p>
-                                <a className="btn btn-gray">PRE-BOOK</a>
-                            </div>
-                            <h4>PRICE:FREE</h4>
-                        </div>
-                    </div>
+            {loading ? <div>loading</div> :
+                <div className="grid-wrapper">
+                    {courses.map((course) =>
+                        <Card title={course.course_name} price={course.price} image={course.thumbnail_url} />
+                    )}
                 </div>
-                <div className="grid-card">
-                    <div className="industry-status">
-                        <a>INDUSTRY</a>
-                        <a>STATUS</a>
-                    </div>
-                    <div className="course-card" >
-                        <img src={background} />
-                        <div>
-                            <div className="course-details">
-                                <p>HAZOP Leadership Training For Engineers</p>
-                                <a className="btn btn-gray">PRE-BOOK</a>
-                            </div>
-                            <h4>PRICE:FREE</h4>
-                        </div>
-                    </div>
-                </div>
-
-                <div className="grid-card">
-                    <div className="industry-status">
-                        <a>INDUSTRY</a>
-                        <a>STATUS</a>
-                    </div>
-                    <div className="course-card" >
-                        <img src={background} />
-                        <div>
-                            <div className="course-details">
-                                <p>HAZOP Leadership Training For Engineers</p>
-                                <a className="btn btn-gray">PRE-BOOK</a>
-                            </div>
-                            <h4>PRICE:FREE</h4>
-                        </div>
-                    </div>
-                </div>
-                <div className="grid-card">
-                    <div className="industry-status">
-                        <a>INDUSTRY</a>
-                        <a>STATUS</a>
-                    </div>
-                    <div className="course-card" >
-                        <img src={background} />
-                        <div>
-                            <div className="course-details">
-                                <p>HAZOP Leadership Training For Engineers</p>
-                                <a className="btn btn-gray">PRE-BOOK</a>
-                            </div>
-                            <h4>PRICE:FREE</h4>
-                        </div>
-                    </div>
-                </div>
-
-
-            </div>
+            }
         </section>
     )
+}
+
+function Card({ image, title, price }) {
+    return <div className="grid-card">
+        <div className="industry-status">
+            <a>INDUSTRY</a>
+            <a>STATUS</a>
+        </div>
+        <div className="course-card" >
+            <img src={image} />
+            <div>
+                <div className="course-details">
+                    <p>{title}</p>
+                    <a className="btn btn-gray">PRE-BOOK</a>
+                </div>
+                <h4>PRICE:{price}</h4>
+            </div>
+        </div>
+    </div>
+
 }
 
 export default Courses
